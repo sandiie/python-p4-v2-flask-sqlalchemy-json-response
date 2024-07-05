d1e91e3a840cd39ac400f5e2785d9f1eac988554
+ <+>
2024-07-05 21:38:53 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -24,8 +24,12 @@ def index():
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet_dict = {
+        'id': 1,
+        'name': 'Fido',
+        'species': 'Dog'
+    }
+    return make_response(pet_dict, 200)
 
 
 if __name__ == '__main__':
